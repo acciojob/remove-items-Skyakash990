5b5c2e98645a5a50ed9c59c19a9a393ccc07eaec
Akash Nandoliya <99115930+Skyakash990@users.noreply.github.com>
2025-02-14 12:54:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let select=document.getElementById("colorSelect");
+
+select.addEventListener("click",()=>{
+	const index = select.selectedIndex;
+	select.remove(index);
+});
